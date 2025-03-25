96373dc5fc40b6b683b295764b4cc842bf5dc604
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-03-25 15:39:43 +0530

    Refactor test for CLMetaData to utilize MetadataExtractor; improve file handling and output formatting

--- test/test_cl_metadata.py
+++ test/test_cl_metadata.py
@@ -1,7 +1,8 @@
 import os
 import time
 import unittest
-from clmediakit.cl_metadata import CLMetaData
+from clmediakit import CLMetaData
+from clmediakit import MetadataExtractor
 
 
 class TestCLMetaData(unittest.TestCase):
@@ -11,45 +12,40 @@ class TestCLMetaData(unittest.TestCase):
         directory_path = "./media"  # os.getenv("MEDIA_DIRECTORY_PATH")
         self.assertIsNotNone(directory_path, "MEDIA_DIRECTORY_PATH is not set")
         self.assertTrue(os.path.isdir(directory_path), "Directory does not exist")
-
-        # Supported media file extensions
-        supported_extensions = {".jpg", ".jpeg", ".png", ".mp4", ".mkv", ".avi"}
-
+        print(f"Processing { directory_path }")
+        extractor = MetadataExtractor()
+        hasTitle = False
         for root, _, files in os.walk(directory_path):
             for file_name in files:
-                file_path = os.path.join(root, file_name)
+                if not file_name.startswith("."):
+                    file_path = os.path.join(root, file_name)
 
-                with self.subTest(file=file_path):
-                    # Start timing
-                    start_time = time.time()
+                    with self.subTest(file=file_path):
 
-                    # Extract metadata
-                    metadata = CLMetaData.from_media(file_path)
+                        # Extract metadata
+                        metadata = CLMetaData.from_media(file_path, extractor=extractor)
 
-                    # End timing
-                    end_time = time.time()
+                        # Assertions to confirm output
+                        self.assertIsNotNone(metadata)
 
-                    # Calculate elapsed time in milliseconds
-                    elapsed_time_ms = (end_time - start_time) * 1000
+                        self.assertTrue(hasattr(metadata, "MIMEType"))
+                        self.assertTrue(hasattr(metadata, "FileSize"))
+                        if metadata.is_image() or metadata.is_video():
+                            self.assertTrue(hasattr(metadata, "dHash"))
+                            self.assertTrue(hasattr(metadata, "md5"))
+                            self.assertTrue(hasattr(metadata, "CreateDate"))
+                            self.assertTrue(hasattr(metadata, "ImageHeight"))
+                            self.assertTrue(hasattr(metadata, "ImageWidth"))
+                        if metadata.is_video():
+                            self.assertTrue(
+                                hasattr(metadata, "Duration")
+                            )  # Example field
 
-                    # Assertions to confirm output
-                    self.assertIsNotNone(metadata)
-
-                    self.assertTrue(hasattr(metadata, "MIMEType"))
-                    self.assertTrue(hasattr(metadata, "FileSize"))
-                    if metadata.is_image() or metadata.is_video():
-                        self.assertTrue(hasattr(metadata, "dHash"))
-                        self.assertTrue(hasattr(metadata, "md5"))
-                        self.assertTrue(hasattr(metadata, "CreateDate"))
-                        self.assertTrue(hasattr(metadata, "ImageHeight"))
-                        self.assertTrue(hasattr(metadata, "ImageWidth"))
-                    if metadata.is_video():
-                        self.assertTrue(hasattr(metadata, "Duration"))  # Example field
-
-                    # Print performance
-                    print(
-                        f"Metadata extraction for {file_path} took {elapsed_time_ms:.2f} ms"
-                    )
+                        # Print performance
+                        if not hasTitle:
+                            print(f"{metadata.keys()}")
+                            hasTitle = True
+                        print(f"{metadata.values()}")
 
 
 if __name__ == "__main__":
